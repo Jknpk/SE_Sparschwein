55f2cb68e233698b1572ac28f1c5593be6f9e2a1
Jannis <j.konopka@fz-juelich.de>
2015-10-13 17:39:17 +0200

    Daten nun von der Konsole einlesen

--- src/application/accounting/MainClass.java
+++ src/application/accounting/MainClass.java
@@ -13,44 +13,62 @@ public class MainClass{
 		
 		//System.out.println(System.getProperty("user.dir"));
 		
-		Scanner sc = new Scanner(System.in);
-
-		
-		while(true){
-		
-			System.out.println("Geben Sie einen Zinssatz an! (Prozent)");
-			int zinssatz = sc.nextInt();
-			System.out.println("Dateipfad zur Eingabedatei: (relativ zur build.xml)\nBei der Eingabe von 'default' wird von src/data/input.csv\n und src/data/output.csv ausgegangen");
-			String dateipfad_input = sc.next();
-			String dateipfad_output;
-			if(dateipfad_input.equals("default")){
-			dateipfad_input = "src/data/input.csv";
-			dateipfad_output = "src/data/output.csv";
-			}
-			else{
-				System.out.println("Dateipfad zur Ausgabedatei: (relativ zur build.xml)");
-				dateipfad_output = sc.next();
-			}
-			try{
-			
-				Buchhaltung neuesBuch = new Buchhaltung(zinssatz, dateipfad_input, dateipfad_output);
-				System.out.println("done successfully");
-				break;
-			}
-			catch(IOException e){
-				System.out.println("Ihre Eingaben waren nicht Korrekt! Versuchen wir es erneut..");
-				continue;
-			}
-			
-			
-			
-		
+		if(args.length != 0){
+			ArgParser parser = new ArgParser(args);
+				try{
+						Buchhaltung neuesBuch = new Buchhaltung(Double.parseDouble(parser.getNonOptions()), parser.getInputFilename(), parser.getOutputFilename());
+						System.out.println("done successfully");
+					}
+					catch(IOException e){
+						System.out.println("Parameter waren nicht korrekt!");
+					}	
+						
 		}
+		else{
 		
-		
-		
-		sc.close();
-		
+			// System.out.println(args.length);
+			
+			Scanner sc = new Scanner(System.in);
+
+			
+			while(true){
+			
+				System.out.println("Geben Sie einen Zinssatz an! (Prozent)");
+				int zinssatz = sc.nextInt();
+				System.out.println("Dateipfad zur Eingabedatei: (relativ zur build.xml)\nBei der Eingabe von 'default' wird von src/data/input.csv\n und src/data/output.csv ausgegangen");
+				String dateipfad_input = sc.next();
+				String dateipfad_output;
+				
+				if(dateipfad_input.equals("default")){
+				dateipfad_input = "src/data/input.csv";
+				dateipfad_output = "src/data/output.csv";
+				}
+				else{
+					System.out.println("Dateipfad zur Ausgabedatei: (relativ zur build.xml)");
+					dateipfad_output = sc.next();
+				}
+				
+				
+				try{
+				
+					Buchhaltung neuesBuch = new Buchhaltung(zinssatz, dateipfad_input, dateipfad_output);
+					System.out.println("done successfully");
+					break;
+				}
+				catch(IOException e){
+					System.out.println("Ihre Eingaben waren nicht Korrekt! Versuchen wir es erneut..");
+					continue;
+				}
+				
+				
+				
+			
+			}
+			
+			
+			
+			sc.close();
+		}
 	}	
 	
 }
