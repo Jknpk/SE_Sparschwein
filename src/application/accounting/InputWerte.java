f197df1a6a331936059c7971ad8217eedbc3dce8
Jannis <j.konopka@fz-juelich.de>
2015-10-22 10:30:16 +0200

    MainClass sieht gut aus, Logging funzt und neue Input Klasse ist dazu gekommen.. Die Kümmert sich ums Einlesen der Werte

---
+++ src/application/accounting/InputWerte.java
@@ -0,0 +1,61 @@
+package application.accounting;
+import java.util.Scanner;
+public class InputWerte{
+
+	private double zinssatz = 0;
+	private String inputData = "";
+	private String outputData = "";
+	private String log = "";
+
+
+	public InputWerte(){
+
+	}
+
+	public void tastaturEingabe(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Geben Sie einen Zinssatz an! (Prozent)");
+		setZinssatz(sc.nextDouble());
+		System.out.println("Dateipfad zur Eingabedatei: (relativ zur build.xml)\nBei der Eingabe von 'default' wird von\n./src/data/input.csv als Eingabedatei\n./src/data/output.csv als Ausgabedatei\n./log.txt als Log-Datei ausgegangen");
+		setInputData(sc.next());
+		if(inputData.equals("default")){
+			setInputData("src/data/input.csv");
+			setOutputData("src/data/output.csv");
+			setLog("./log.txt");
+		}
+		else{
+			System.out.println("Dateipfad zur Ausgabedatei: (relativ zur build.xml)");
+			setOutputData(sc.next());
+			System.out.println("Dateipfad zur Log Datei: (relativ zur build.xml), default ist ./log.txt");
+			setLog(sc.next());
+		}
+		sc.close();	
+	}
+
+	
+	public double getZinssatz(){
+		return zinssatz;
+	}
+	public void setZinssatz(double zinssatz_eingabe){
+		 this.zinssatz = zinssatz_eingabe;
+	}
+	public String getInputData(){
+		return inputData;
+	}
+	public void setInputData(String inputData_eingabe){
+		 this.inputData = inputData_eingabe;
+	}
+	public String getOutputData(){
+		return outputData;
+	}
+	public void setOutputData(String outputData_eingabe){
+		 this.outputData = outputData_eingabe;
+	}
+	public String getLog(){
+		return log;
+	}
+	public void setLog(String log_eingabe){
+		 this.log = log_eingabe;
+	}
+
+}
--- src/application/accounting/MainClass.java
+++ src/application/accounting/MainClass.java
@@ -1,107 +1,66 @@
 package application.accounting;
-import java.util.Scanner;
 import java.io.IOException;
 import java.util.logging.*;
 
-
-
 public class MainClass{
+	
 
 	protected static final Logger logger = Logger.getLogger(Buchhaltung.class.getName());
 	
-
-
 	public static void main(String[] args){
-	  logger.setLevel(Level.ALL);
+	// Vorinitialisierung der Übergabe-Parameter
 	
-	  try{
-	    String log = parser.getLogger();
-	    boolean append = true;
-	    FileHandler fh = new FileHandler(log, append);
-	    fh.setFormatter(new Formatter() {
-	      public String format(LogRecord rec) {
-		StringBuffer buf = new StringBuffer(1000);
-		buf.append(new java.util.Date()).append(' ');
-		buf.append(rec.getLevel()).append(' ');
-		buf.append(formatMessage(rec)).append('\n');
-		return buf.toString();
-	      }
-	    
-	    });
-	  logger.addHandler(fh);
-	  	  
-	  } catch(IOException e){
-	    logger.severe("Datei kann nicht geschrieben werden");
-	    e.printStackTrace();
-	  
-	  }
-	  
-	  
-	  
-	  logger.warning("ICH WILL WAS SEHEN, TITEN AUF DEN TISCH, Butter am Fisch!");
-	  
-	  
-	
-		
-		if(args.length != 0){
-			ArgParser parser = new ArgParser(args);
-				try{
-						logger.info("lese von Datei: " + parser.getInputFilename());
-						Buchhaltung neuesBuch = new Buchhaltung(Double.parseDouble(parser.getNonOptions()), parser.getInputFilename(), parser.getOutputFilename());
-						System.out.println("done successfully");
-					}
-					catch(IOException e){
-						logger.severe("Alles scheisse, Leben hat kein Sinn mehr 1");
-						System.out.println("Parameter waren nicht korrekt!");
-					}	
-						
-		}
-		else{
-		
-			// System.out.println(args.length);
-			
-			Scanner sc = new Scanner(System.in);
+	InputWerte eingabe = new InputWerte();
+	if(args.length != 0){
+	// Eingabe der Parameter erfolgt über XML-Datei
+		ArgParser parser = new ArgParser(args);
+		eingabe.setZinssatz(Double.parseDouble(parser.getNonOptions()));
+		eingabe.setInputData(parser.getInputFilename());
+		eingabe.setOutputData(parser.getOutputFilename());
+		eingabe.setLog(parser.getLogFilename());		
+	}
+	else{
+	// Eingabe der Parameter erfolgt über Tastatur
+		eingabe.tastaturEingabe();
+	}
 
-			
-			while(true){
-			
-				System.out.println("Geben Sie einen Zinssatz an! (Prozent)");
-				int zinssatz = sc.nextInt();
-				System.out.println("Dateipfad zur Eingabedatei: (relativ zur build.xml)\nBei der Eingabe von 'default' wird von src/data/input.csv\n und src/data/output.csv ausgegangen");
-				String dateipfad_input = sc.next();
-				String dateipfad_output;
-				
-				if(dateipfad_input.equals("default")){
-				dateipfad_input = "src/data/input.csv";
-				dateipfad_output = "src/data/output.csv";
-				}
-				else{
-					System.out.println("Dateipfad zur Ausgabedatei: (relativ zur build.xml)");
-					dateipfad_output = sc.next();
-				}
-				
-				
-				try{
-					logger.info("lese von Datei: " + dateipfad_input);
-					Buchhaltung neuesBuch = new Buchhaltung(zinssatz, dateipfad_input, dateipfad_output);
-					System.out.println("done successfully");
-					break;
-				}
-				catch(IOException e){
-					logger.severe("Alles scheisse, Leben hat kein Sinn mehr 2");
-					System.out.println("Ihre Eingaben waren nicht Korrekt! Versuchen wir es erneut..");
-					continue;
-				}
-				
-				
-				
-			
-			}
-			
-			
-			
-			sc.close();
+	// Initialisierung des Loggers	
+	logger.setLevel(Level.ALL);
+	
+	try{
+	boolean append = true;
+	FileHandler fh = new FileHandler(eingabe.getLog(), append);
+	fh.setFormatter(new Formatter() {
+		public String format(LogRecord rec) {
+			StringBuffer buf = new StringBuffer(1000);
+			buf.append(new java.util.Date()).append(' ');
+			buf.append(rec.getLevel()).append(' ');
+			buf.append(formatMessage(rec)).append('\n');
+			return buf.toString();
 		}
-	}	
+	});
+	logger.addHandler(fh);
+	    
+	} catch(IOException e){
+		logger.severe("Datei kann nicht geschrieben werden");
+		e.printStackTrace();
+	}
+	   
+	// Durchführung der Buchhaltung mit den gelesenen Parametern.   
+	while(true){
+		try{
+			logger.info("lese von Datei: " + eingabe.getLog());
+			Buchhaltung neuesBuch = new Buchhaltung(eingabe.getZinssatz(), eingabe.getInputData(), eingabe.getOutputData());
+			System.out.println("done successfully");
+			break;
+		}
+		catch(IOException e){
+			logger.severe("Es ist wohl ein Fehler aufgetreten..");
+			System.out.println("Parameter waren nicht korrekt!");
+			eingabe.tastaturEingabe();
+		}	
+	}
+
+	}
 	
 }
